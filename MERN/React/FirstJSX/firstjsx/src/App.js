9eb8946c5fcbf4507e01b9f5fdc918c90c3cd18c
albinadervishi <albinadervishi0@gmail.com>
2023-04-19 00:00:59 +0200

    new

--- MERN/React/FirstJSX/firstjsx/src/App.js
+++ MERN/React/FirstJSX/firstjsx/src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PersonCard from '../../../PropItUp/client/src/components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello Dojo!</h1>
-        <h2>Things i need to do:</h2>  
-        <ul>
-          <li>Learn React</li>
-          <li>Climb Mt. Everest</li>
-          <li>Run a marathon</li>
-          <li>Feed the dogs</li>
-        </ul>
+        <PersonCard />
     </div>
   );
 }
