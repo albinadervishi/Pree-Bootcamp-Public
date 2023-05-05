60defcf2145d4e87bf29a611852db1bd6c741ffc
albinadervishi <albinadervishi0@gmail.com>
2023-05-05 12:42:05 +0200

    new

--- MERN/React/pokemon/src/App.js
+++ MERN/React/pokemon/src/App.js
@@ -6,7 +6,7 @@ function App() {
   
     const [pokemonList, setPokemonList] = useState([]);
     useEffect(() => {
-      fetch("https://pokeapi.co/api/v2/pokemon/")
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
             .then(response => response.json())
             .then(data => {
                 setPokemonList(data.results);
